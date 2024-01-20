427b33b4a91fc25661b4da3c763ce989f24941c2
eswai <sakamoto.yuki@gmail.com>
2024-01-20 12:34:10 +0900

    copy from qmk https://github.com/eswai/qmk_firmware

---
+++ keyboards/eswai/kunai16/config.h
@@ -0,0 +1,45 @@
+/*
+<<<<<<<< HEAD:keyboards/eswai/kunai16/config.h
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+========
+Copyright 2019 iw0rm3r
+>>>>>>>> 42c6920e23d8dace7652155d04310cae60f12e42:keyboards/ibm/model_m/teensy2/config.h
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* The Model M has no diodes */
+#define MATRIX_HAS_GHOST
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+
